29d9b29f21f5e4a9f3a4c965c34616c72272092b
Verma <kritul.verma@capgemini.com>
2026-01-22 14:59:31 +0530

    Added ProtectedRoutes using Authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { Routes,Route, Navigate} from 'react-router-dom';
 import Login from './pages/Login.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Login/>
-    </>
+
+    <Routes>
+      {/* public Route */}
+      <Route path="/login" element={<Login/>}/>
+      {/* protected Route */}
+      <Route path="/dashboard" element=
+        {
+          <ProtectedRoute>
+            <h1>Dashboard - Protected Content</h1>
+          </ProtectedRoute>
+        }
+      />
+      {/* redirect unknown routes to login */}
+      <Route path="*" element={<Navigate to="/login" replace/>}/>
+    </Routes>
   )
 }
 
